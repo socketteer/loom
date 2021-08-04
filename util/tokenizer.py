f1bf0eb48646b694504e0730cf0efeb2503b71e7
Airal <lreyn@umich.edu>
2021-08-04 00:29:04 -0400

    tokenizer imported only when needed

--- util/tokenizer.py
+++ util/tokenizer.py
@@ -1,15 +1,21 @@
 from transformers import GPT2Tokenizer
 
-#tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
 
 def tokenize(input):
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     return tokenizer(input)['input_ids']
 
+
 def detokenize(tokens):
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     return tokenizer.convert_tokens_to_string(tokens)
 
+
 def token_to_word(token):
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     return tokenizer.convert_ids_to_tokens([token])[0]
+
 
 def logit_mask(mask):
     id_mask = {}
