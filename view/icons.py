18d7ce565cd7f7781753b74592c44276c5736176
metasemi <mike@michael-brian-orr.net>
2023-03-27 08:13:24 -0700

    fix and re-enable tree visualization

--- view/icons.py
+++ view/icons.py
@@ -10,15 +10,20 @@ class Icons:
     def init_icon(self, icon_name, filename, size=16):
         self.icons[icon_name] = {}
         self.icons[icon_name]["size"] = size
+        self.icons[icon_name]["zsize"] = size
         self.icons[icon_name]["img"] = PIL.Image.open(f"./static/icons/{filename}")
         with open(f"./static/icons/{filename}", "rb") as f:
             img = PIL.Image.open(io.BytesIO(f.read()))
             self.icons[icon_name]["img"] = img
         # self.icons[icon_name]["img"] = PIL.Image.open(f"./static/icons/{filename}")
 
-    def get_icon(self, icon_name, size=16):
-        if 'icon' not in self.icons[icon_name]:
-            self.icons[icon_name]['icon'] = PIL.ImageTk.PhotoImage(self.icons[icon_name]['img'].resize((size, size)))
+    # zsize: zoomed size, physical size of image to be cached and returned
+    def get_icon(self, icon_name, zsize=None):
+        if 'icon' not in self.icons[icon_name] or (zsize is not None and zsize != self.icons[icon_name]["zsize"]):
+            if zsize is None:
+                zsize = self.icons[icon_name]["zsize"]
+            self.icons[icon_name]['icon'] = PIL.ImageTk.PhotoImage(self.icons[icon_name]['img'].resize((zsize, zsize)))
+            self.icons[icon_name]["zsize"] = zsize
         return self.icons[icon_name]['icon'] 
 
     def init_icons(self):
